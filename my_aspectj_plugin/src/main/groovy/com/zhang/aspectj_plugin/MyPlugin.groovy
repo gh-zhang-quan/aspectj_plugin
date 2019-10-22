4eca753cf52f85c570d811eeee56b10678ffd8af
kuencheung_admin <zhang_quan_888@163.com>
2019-10-22 14:07:47 +0800

    aspectj-plugin插件初次提交

---
+++ my_aspectj_plugin/src/main/groovy/com/zhang/aspectj_plugin/MyPlugin.groovy
@@ -0,0 +1,76 @@
+package com.zhang.aspectj_plugin
+
+import com.android.build.gradle.AppPlugin
+import com.android.build.gradle.LibraryPlugin
+import org.aspectj.bridge.IMessage
+import org.aspectj.bridge.MessageHandler
+import org.aspectj.tools.ajc.Main
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.tasks.compile.JavaCompile
+
+class MyPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        def hasApp = project.plugins.withType(AppPlugin)
+        def hasLib = project.plugins.withType(LibraryPlugin)
+        if (!hasApp && !hasLib) {
+            throw new IllegalStateException("'application' or 'library' plugin required")
+        }
+        final def log = project.logger
+        final def variants
+        if (hasApp) {
+            variants = project.android.applicationVariants
+        } else {
+            variants = project.android.libraryVariants
+        }
+        project.dependencies {
+            api 'org.aspectj:aspectjrt:1.9.4'
+        }
+// -showWeaveInfo，输出编织过程信息
+// -1.5 设置规范1.5，匹配java1.5
+// -inpath class文件目录或者jar包， 源字节码，需要处理的类
+// -aspectpath  定义的切面类
+// -d 存放编辑产生的class文件
+// -classpath ，所有class文件，源class，java包，编织时需要用到的一些处理类
+        variants.all { variant ->
+            JavaCompile javaCompile = variant.javaCompile
+            javaCompile.doLast {
+                String[] args = [
+                        "-showWeaveInfo",
+                        "-1.5",
+                        "-inpath", javaCompile.destinationDir.toString(),
+                        "-aspectpath", javaCompile.classpath.asPath,
+                        "-d", javaCompile.destinationDir.toString(),
+                        "-classpath", javaCompile.classpath.asPath,
+                        "-bootclasspath", project.android.bootClasspath.join(File.pathSeparator)
+                ]
+                log.debug "zhang args:" + Arrays.toString(args)
+                MessageHandler handler = new MessageHandler(true)
+                new Main().run(args, handler)
+                for (IMessage message : handler.getMessages(null, true)) {
+                    switch (message.getKind()) {
+                        case IMessage.ABORT:
+                        case IMessage.ERROR:
+                        case IMessage.FAIL:
+                            log.error message.message, message.thrown
+                            break
+                        case IMessage.WARNING:
+                            log.warn message.message, message.thrown
+                            break
+                        case IMessage.INFO:
+                            log.info message.message, message.thrown
+                            break
+                        case IMessage.DEBUG:
+                            log.debug message.message, message.thrown
+                            break
+                    }
+                }
+
+            }
+        }
+    }
+
+
+}
